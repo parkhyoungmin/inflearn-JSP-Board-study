97b178444496bfb24c7e0b7357a4455b791173ef
parkhyoungmin <parkhm94@naver.com>
2021-07-13 14:14:33 +0900

    create Ibs.java, ibs.jsp

---
+++ IBS/src/ibs/Ibs.java
@@ -0,0 +1,49 @@
+package ibs;
+
+public class Ibs {
+
+	private int ibsID;
+	private String ibsTitle;
+	private String userID;
+	private String ibsDate;
+	private String ibsContent;
+	private int ibsAvailable;
+	
+	public int getIbsID() {
+		return ibsID;
+	}
+	public void setIbsID(int ibsID) {
+		this.ibsID = ibsID;
+	}
+	public String getIbsTitle() {
+		return ibsTitle;
+	}
+	public void setIbsTitle(String ibsTitle) {
+		this.ibsTitle = ibsTitle;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getIbsDate() {
+		return ibsDate;
+	}
+	public void setIbsDate(String ibsDate) {
+		this.ibsDate = ibsDate;
+	}
+	public String getIbsContent() {
+		return ibsContent;
+	}
+	public void setIbsContent(String ibsContent) {
+		this.ibsContent = ibsContent;
+	}
+	public int getIbsAvailable() {
+		return ibsAvailable;
+	}
+	public void setIbsAvailable(int ibsAvailable) {
+		this.ibsAvailable = ibsAvailable;
+	}
+	
+}
